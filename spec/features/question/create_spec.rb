1629995867364f5dea9ec5027d2e65494531d84b
histfak <histfak@gmail.com>
2019-06-20 21:22:50 +0300

    theory part

---
+++ spec/features/question/create_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+feature 'User can create a question' do
+  given(:user) {create(:user)}
+
+  describe 'Authenticated user' do
+
+    background do
+      login(user)
+      visit questions_path
+      click_on 'Ask question'
+    end
+
+    scenario 'asks a question' do
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'text text text'
+      click_on 'Ask'
+
+      expect(page).to have_content 'Your question has been successfully created.'
+      expect(page).to have_content 'Test question'
+      expect(page).to have_content 'text text text'
+    end
+
+    scenario 'asks a question with errors' do
+      click_on 'Ask'
+
+      expect(page).to have_content "Title can't be blank"
+    end
+  end
+
+  scenario 'Unauthenticated user tries to ask a question' do
+    visit questions_path
+    click_on 'Ask question'
+
+    expect(page).to have_content 'You need to sign in or sign up before continuing.'
+  end
+end
