35ed638ac64e9b90edfa1e2cffef945b3802dda4
histfak <histfak@gmail.com>
2019-07-06 02:58:13 +0300

    first two tasks

--- spec/features/answer/add_link_spec.rb
+++ spec/features/answer/add_link_spec.rb
@@ -4,6 +4,7 @@ feature 'User can add links to answer' do
   given(:user) { create(:user) }
   given!(:question) { create(:question) }
   given(:gist_url) { 'https://gist.github.com/histfak/ba9a3358cbe5e99563b7a56b5ae0dc4a' }
+  given(:invalid_url) { 'foobar' }
 
   scenario 'User adds link when asks answer', js: true do
     login(user)
@@ -20,6 +21,24 @@ feature 'User can add links to answer' do
 
     within '.answers' do
       expect(page).to have_link 'My gist', href: gist_url
+    end
+  end
+
+  scenario 'User adds invalid link when asks answer', js: true do
+    login(user)
+    visit question_path(question)
+
+    within '.new-answer' do
+      fill_in 'Your answer:', with: 'new answer body'
+
+      fill_in 'Link name', with: 'My gist'
+      fill_in 'URL', with: invalid_url
+
+      click_on 'Post answer'
+    end
+
+    within '.answer-errors' do
+      expect(page).to have_content 'Links url is invalid'
     end
   end
 end
--- spec/features/question/add_link_spec.rb
+++ spec/features/question/add_link_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature 'User can add links to question' do
   given(:user) { create(:user) }
   given(:gist_url) { 'https://gist.github.com/histfak/ba9a3358cbe5e99563b7a56b5ae0dc4a' }
+  given(:invalid_url) { 'foobar' }
 
   scenario 'User adds link when asks question' do
     login(user)
@@ -17,5 +18,21 @@ feature 'User can add links to question' do
     click_on 'Ask'
 
     expect(page).to have_link 'My gist', href: gist_url
+  end
+
+
+  scenario 'User adds invalid link when asks question' do
+    login(user)
+    visit new_question_path
+
+    fill_in 'Title', with: 'Test question'
+    fill_in 'Body', with: 'text text text'
+
+    fill_in 'Link name', with: 'My gist'
+    fill_in 'URL', with: invalid_url
+
+    click_on 'Ask'
+
+    expect(page).to have_content 'Links url is invalid'
   end
 end
