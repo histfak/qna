eaa66d00621c6017a61c27f76d965030e93ec15e
histfak <histfak@gmail.com>
2019-07-08 11:24:45 +0300

    fix after review

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -16,7 +16,7 @@ class Answer < ApplicationRecord
     transaction do
       question.answers.where(best: true).update_all(best: false)
       update!(best: true)
-      question.reward.update!(user: author) if question.reward.present? && question.author != author
+      question.reward.update!(user: author) if question.reward.present? && !question.author.author?(self)
     end
   end
 end
