66f8f8af5f7b4489bb3eb23a881645f0a40b41ca
histfak <histfak@gmail.com>
2020-04-03 02:37:54 +0300

    caching

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   include Authorable
 
   belongs_to :user
-  belongs_to :commentable, polymorphic: true
+  belongs_to :commentable, polymorphic: true, touch: true
 
   validates :body, presence: true
 end
